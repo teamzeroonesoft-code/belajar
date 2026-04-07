ddd2d89aba46f48d4b23fbca1d26b6af43cff988
dandamiftah <kdanda20@gmail.com>
2026-04-08 01:05:28 +0700

    buat template

---
+++ super/template/head.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+if (empty($_SESSION['login'])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">                                                                                                                                                                                           
+</head>
+<body>
